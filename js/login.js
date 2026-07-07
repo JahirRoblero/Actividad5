0dfb8d510e3556cb4009bb943e4db0318dbc67e9
AlejandroGuillermo7 <ale1111prepa7@gmail.com>
2026-07-07 13:13:08 -0600

    loginv2

--- js/login.js
+++ js/login.js
@@ -42,7 +42,10 @@ botonIniciar.addEventListener("click", function () {
 
     if (correoOk && passwordOk) {
         badgeExito.classList.add("activo");
-    } else {
+        setTimeout(function () {
+            window.location.href = "index.html";
+        }, 1000);
+    } else {    
         badgeExito.classList.remove("activo");
     }
 
