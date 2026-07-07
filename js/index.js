5eb651cd9fd34b372dc1ac0f00cd78f16d2498de
Angel Jahir Gomez Roblero <147016693+JahirRoblero@users.noreply.github.com>
2026-07-07 16:59:07 -0600

    Agregue cambios

--- js/index.js
+++ js/index.js
@@ -16,3 +16,27 @@ function toggleSubmenuUsuarios() {
     flecha.textContent = "▼";
   }
 }
+
+function mostrarApartado(apartado) {
+  const bienvenida = document.getElementById("bienvenida");
+  const formCapturaUsuario = document.getElementById("formCapturaUsuario");
+  const formValidarAlumno = document.getElementById("formValidarAlumno");
+  const titulo = document.querySelector("header h1");
+
+  bienvenida.classList.add("oculto");
+  formCapturaUsuario.classList.add("oculto");
+  formValidarAlumno.classList.add("oculto");
+
+  if (apartado === "captura") {
+    formCapturaUsuario.classList.remove("oculto");
+    titulo.textContent = "Captura de usuarios";
+  }
+
+  if (apartado === "alumnos") {
+    formValidarAlumno.classList.remove("oculto");
+    titulo.textContent = "Validar alumnos";
+  }
+
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.remove("activo");
+}
